9009d9ae0cdc2938ab0d4146489f99273e21b20e
anemonanetedu <nicol.netedu@student.upt.ro>
2022-04-04 18:56:39 +0300

    Login Form Update

--- anterestproject/src/App.js
+++ anterestproject/src/App.js
@@ -1,24 +1,36 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import logoanterest from './logoanterest.jpeg';
+import Login from './Pages/Login';
+import {BrowserRouter} from 'react-router-dom';
+import { Switch } from 'react-router-dom'
+
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logoanterest} className="App-logo" alt="logo" />
+    <div>
+    <header className="App-header">
+            <img src={logoanterest} className="App-logo" alt="logo" />
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+      <Router>
+      <Switch>
+
+      <Route path='/Login'component={Login}/>
+
+      </Switch>
+      </Router>
     </div>
   );
 }
