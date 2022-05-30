ef0f505518eebaf82f2039c9eb343d1a2103c65d
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2022-05-30 15:00:31 +0300

    Footer done

--- anterestproject/src/Components/Footer.js
+++ anterestproject/src/Components/Footer.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
   <div className="Footer">
-    <p>Footer content</p>
+    <h4>&copy; 2022 IC Project</h4>
+    <h4>Muntean Andra & Netedu Anemona</h4>
+    <h3>Social Follow</h3>
+            <a href=""
+              className="youtube social">
+              <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </a>
+             <a href=""
+                className="facebook social">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+             </a>
+             <a href=""
+                className="twitter social">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+             </a>
+             <a href=""
+                className="instagram social">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+             </a>
   </div>
+
+
 );
 
 export default Footer;
