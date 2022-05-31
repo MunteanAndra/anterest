2d4f2a70124da5b53c0f45bc04155250bcbad8e5
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2022-05-31 19:03:03 +0300

    Changed style for SearchBar

--- anterestproject/src/Components/SearchBar.js
+++ anterestproject/src/Components/SearchBar.js
@@ -11,9 +11,11 @@ class SearchBar extends React.Component {
 
     render () {
         return (
+
             <form onSubmit={this.onFormSubmit} className="ui form">
+            <script src="https://kit.fontawesome.com/afd6aa68df.js" crossorigin="anonymous"></script>
             <div className="field">
-            <input type="text" placeholder="Search for photos on page..." value={this.state.term} onChange={(e) => this.setState({ term: e.target.value})}/>
+            <input type="text" placeholder="   Search for photos on page..." value={this.state.term} onChange={(e) => this.setState({ term: e.target.value})}/>
             </div>
             </form>
             );
